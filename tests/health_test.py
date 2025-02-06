15dfe5e980ee61d34d2c6ad6f017106ef5c128a4
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2025-02-06 22:56:09 +0100

    got difficul. going step by step.

--- tests/health_test.py
+++ tests/health_test.py
@@ -5,12 +5,15 @@ def test_open_xterm():
     # Configura a variável DISPLAY para usar o ambiente virtual do Xvfb
     os.environ["DISPLAY"] = ":99"  # Isso garante que o xterm será exibido no display virtual do Xvfb
 
-    # Tenta abrir o terminal diretamente com o comando "xterm"
-    os.system("xterm &")  # Abre o terminal xterm diretamente
-    time.sleep(5)  # Espera o terminal abrir
-
-    # Exibe uma mensagem de sucesso
-    print("Terminal xterm foi aberto.")
+    # Verifica se o processo xterm já está em execução
+    existing_process = os.popen("pgrep xterm").read()
+    if not existing_process:
+        # Abre o terminal xterm diretamente
+        os.system("xterm &")  # Abre o terminal xterm diretamente
+        time.sleep(5)  # Espera o terminal abrir
+        print("Terminal xterm foi aberto.")
+    else:
+        print("xterm já está em execução.")
 
 # Execute a função para testar
 test_open_xterm()
