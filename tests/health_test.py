404dbfe91cfdf191c4ee144bb13eccab1f9484db
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2025-02-06 12:15:29 +0100

    attempt in workflow next

--- tests/health_test.py
+++ tests/health_test.py
@@ -9,8 +9,7 @@ import os
 def test_curl_response():
     # Open Git Bash using subprocess without needing to click
     # subprocess.Popen("start bash.exe", shell=True)  # Opens Terminal configured as Git Bash 
-    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "your-command; exec bash"])  # to commit in github actions workflow
-    # subprocess.Popen(["xterm", "-e", "your-command"]) #other alternative 
+    subprocess.Popen(["xterm", "-hold", "-e", "bash"])
 
     time.sleep(5)  # Wait for the Bash window to open
 
